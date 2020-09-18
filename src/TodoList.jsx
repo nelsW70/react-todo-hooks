5f291a9a38abea89af800da774788d3cb223557c
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-18 15:23:52 -0400

    add: material-ui elements for Todo

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
+import Todo from './Todo';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 
 function TodoList(props) {
   return (
     <Paper>
       <List>
-        {props.todos.map(todo => (
-          <>
-            <ListItem>
-              <ListItemText>{todo.task}</ListItemText>
-            </ListItem>
+        {props.todos.map((todo, i) => (
+          <React.Fragment key={i}>
+            <Todo task={todo.task} key={todo.id} completed={todo.completed} />
             <Divider />
-          </>
+          </React.Fragment>
         ))}
       </List>
     </Paper>
